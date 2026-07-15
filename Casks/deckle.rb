469a1f0812b9972d75d3cbb2b4035853e353a32e
YellowFoxH4XOR <yellowfoxh4xor@gmail.com>
2026-07-15 21:57:00 +0530

    Add Deckle cask (v1.1.0)

---
+++ Casks/deckle.rb
@@ -0,0 +1,24 @@
+cask "deckle" do
+  version "1.1.0"
+  sha256 "3c827ace47b016cde4f6aa07001c46a87b6352dd5e9dc3d3abdd656aa1d3c237"
+
+  url "https://github.com/YellowFoxH4XOR/deckle/releases/download/v#{version}/Deckle-#{version}.dmg"
+  name "Deckle"
+  desc "Paper-grain texture overlay that makes your screen feel like paper"
+  homepage "https://github.com/YellowFoxH4XOR/deckle"
+
+  depends_on macos: ">= :ventura"
+
+  app "Deckle.app"
+
+  caveats <<~EOS
+    Deckle is open source and not notarized. If macOS blocks the first
+    launch, right-click Deckle.app in /Applications and choose Open,
+    or reinstall with:
+      brew reinstall --cask deckle --no-quarantine
+  EOS
+
+  zap trash: [
+    "~/Library/Preferences/app.deckle.Deckle.plist",
+  ]
+end
